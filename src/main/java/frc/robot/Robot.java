24d7b0cbe66073c5b8712c004ab23c350b05a4f7
JamerGamer <Jfriggs1877@gmail.com>
2022-01-22 13:03:43 -0500

    added autonomous selector

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.Timer;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
@@ -21,7 +20,6 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.smartdashboard.*;
 
 
  /* This is a demo program showing the use of the RobotDrive class, specifically it contains the code
